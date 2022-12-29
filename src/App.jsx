8e0102b380658e0c8f7f7e7e0685aadc014e9e16
Jingshun <jingshun.yang.cs@gmail.com>
2022-12-29 14:51:09 +0800

    feat: updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { Suspense, lazy } from "react"
 import { Link, Routes, Route } from "react-router-dom"
-// import Home from "./pages/Home"
-// import About from "./pages/About"
+import { Button } from "antd"
 const Home = lazy(() => import(/* webpackChunkName: 'home' */ "./pages/Home"))
 const About = lazy(() => import(/* webpackChunkName: 'about' */ "./pages/About"))
 
 function App() {
   return <div>
     <h1>App</h1>
+    <Button type="primary">按钮</Button>
     <ul>
       <li>
         <Link to="/home">Home</Link>
